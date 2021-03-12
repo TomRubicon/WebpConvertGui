511fdda8f420dd145b69e51cbae120723e82ccaf
John <johnmilanowski@gmail.com>
2021-03-12 11:38:15 -0800

    Add barebones ui created with pyside6-designer

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
 from PySide6.QtWidgets import QApplication, QMainWindow
+from ui_mainwindow import Ui_MainWindow
 
 class MainWindow(QMainWindow):
     def  __init__(self):
         super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -12,4 +15,3 @@ if __name__ == '__main__':
     window.show()
 
     sys.exit(app.exec_())
-    
