082158eb5ec06f4a6e317b19dc5d489379ef70a7
John <johnmilanowski@gmail.com>
2021-03-13 16:15:01 -0800

    Prepare MainWindow class to handle file conversion

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WEBP Batch Conversion GUI
-More to come here at a later date!
+
+Make sure you download gif2webp/webpmux and place them in the script directory!
+-[Download webp tools here!](https://storage.googleapis.com/downloads.webmproject.org/releases/webp/index.html)
+-[Webp tools documentation](https://developers.google.com/speed/webp/docs/using)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
-from glob import glob
 import os
-from os import listdir
+import subprocess
+from os import listdir, path
 from os.path import isfile, join
 from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
 from ui_mainwindow import Ui_MainWindow
@@ -13,36 +13,83 @@ class MainWindow(QMainWindow):
         self.ui.setupUi(self)
         self.assign_widgets()
 
+        self.input_dir = ''
+        self.output_dir = ''
+
+        self.thumbnail_suffix = ''
+
+        self.gifs = []
+
+        self.gif2webp_args = []
+
+        self.gif2webp = 'gif2webp.exe'
+        self.webpmux = 'webpmux.exe'
+
+        if path.isfile('gif2webp.exe') and path.isfile('webpmux.exe'):
+            self.ui.lbl_webpinstallwarning.setText('')
+
+
     def assign_widgets(self):
-        self.ui.btn_selectdir.clicked.connect(self.set_dir)
+        self.ui.btn_selectdir.clicked.connect(self.set_input_dir)
         self.ui.btn_selectoutputdir.clicked.connect(self.set_output_dir)
+        self.ui.btn_convert.clicked.connect(self.convert_gifs)
     
-    def set_dir(self):
+    def set_input_dir(self):
         os.chdir(str(QFileDialog.getExistingDirectory(self, 'Select Directory')))
+
         self.ui.txt_workingdir.setText(os.getcwd())
-        self.ui.txt_outputdir.setEnabled(True)
-        self.ui.btn_selectoutputdir.setEnabled(True)
+        
         self.ui.txt_outputdir.setText(os.getcwd())
+        self.output_dir = os.getcwd()
 
         path = os.getcwd()
-        files = []
         
-        for f in os.listdir(path):
+        for f in listdir(path):
             if f.endswith('.gif'):
-                files.append(f)
+                self.gifs.append(f)
         
         self.ui.txt_dircontents.setEnabled(True)
         self.ui.txt_dircontents.setText('')
 
-        self.ui.lbl_gifcount.setText(str(len(files)))
+        self.ui.lbl_gifcount.setText(str(len(self.gifs)))
 
-        for f in files:
+        self.enable_widgets()
+
+        for f in self.gifs:
             self.ui.txt_dircontents.append(f)
-    
+        
+        print('self.gifs output:', self.gifs)
+
+    def enable_widgets(self):
+        self.ui.txt_outputdir.setEnabled(True)
+        self.ui.btn_selectoutputdir.setEnabled(True)
+
+        self.ui.btn_convert.setEnabled(True)
+        #Labels
+        self.ui.lbl_compressionmode.setEnabled(True)
+        self.ui.lbl_lossyfiltering.setEnabled(True)
+        self.ui.lbl_quality.setEnabled(True)
+        #Check Boxes
+        self.ui.chb_minsize.setEnabled(True)
+        self.ui.chb_makethumbs.setEnabled(True)
+        self.ui.chb_deletegifs.setEnabled(True)
+        #Combo box
+        self.ui.cmb_compressionmode.setEnabled(True)
+        #Text boxes
+        self.ui.txt_lossyfiltering.setEnabled(True)
+        self.ui.txt_quality.setEnabled(True)
+
     def set_output_dir(self):
-        set_dir = str(QFileDialog.getExistingDirectory(self, 'Select Directory'))
-        set_dir = os.path.normpath(set_dir)
-        self.ui.txt_outputdir.setText(set_dir)
+        self.output_dir = str(QFileDialog.getExistingDirectory(self, 'Select Directory'))
+        self.output_dir = os.path.normpath(self.output_dir)
+        self.ui.txt_outputdir.setText(self.output_dir)
+        print('Output dir set to', self.output_dir)
+    
+    def convert_gifs(self):
+        self.ui.txt_convertlog.setEnabled(True)
+        self.ui.progressBar.setEnabled(True)
+        print(self.ui.chb_makethumbs.isChecked())
+        print('Processing!')
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
